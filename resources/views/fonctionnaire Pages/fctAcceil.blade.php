79ab9d977ee8c6a152f78cf677e8a90f52cd6144
OUSSAMA HADAN <oussamaheddane1@gmail.com>
2025-02-04 18:36:03 -0800

    fct acceil fixed

--- resources/views/fonctionnaire Pages/fctAcceil.blade.php
+++ resources/views/fonctionnaire Pages/fctAcceil.blade.php
@@ -9,19 +9,28 @@
     {{-- @vite('resources/css/app.css') --}}
 </head>
 <body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-8 d-flex">
-                <input type="text" class="form-control me-2" placeholder="Search...">
+    <div class="container mt-5 d-flex justify-content-center">
+        <div class="row w-100">
+            <div class="col-md-8 d-flex justify-content-end">
+            <input type="text" class="form-control me-2 flex-grow-1" placeholder="Search...">
+            <button class="btn btn-primary d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zm-5.442 1.398a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
+                </svg>
+            </button>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-3 d-flex justify-content-center">
+        <div class="row w-100">
+            <div class="col-md-8 d-flex justify-content-end align-items-center">
+                <label for="fromDate" class="me-2">From:</label>
+                <input type="date" id="fromDate" class="form-control me-2" placeholder="From Date">
+                <label for="toDate" class="me-2">To:</label>
+                <input type="date" id="toDate" class="form-control me-2" placeholder="To Date">
                 <button class="btn btn-primary d-flex align-items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zm-5.442 1.398a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
-                    </svg>
+                    Filter
                 </button>
-            </div>
-            <div class="col-md-4 text-end">
-                <button class="btn btn-success">Nouveau Ordre de Mission</button>
-            </div>
             </div>
         </div>
     </div>
