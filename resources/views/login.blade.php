57453a090a9ec7da55ffb2604f0180c75836a71d
OUSSAMA HADAN <oussamaheddane1@gmail.com>
2025-02-05 01:15:53 -0800

    Remove unused Bootstrap CSS and JS links from navigation, sidebar, and login views

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,6 @@
     <title>Authentification</title>
     <!-- Bootstrap CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <!-- Custom Styles -->
     <style>
         .bg-cover {
@@ -61,7 +60,5 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS (with Popper.js) -->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> --}}
 </body>
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,3 @@
-
-<!-- Bootstrap CSS -->
-{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <style>
     .navbar {
@@ -16,9 +13,7 @@
         font-size: 18px;
         font-weight: 500;
     }
-    span{
-        color: white;
-    }
+    
 </style>
 <body>
 
