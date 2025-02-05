7e8b69554437b6a9a8289c81e47da1b817a52df1
OUSSAMA HADAN <oussamaheddane1@gmail.com>
2025-02-04 23:46:26 -0800

    Enhance navbar and sidebar styles for improved aesthetics and consistency

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -22,18 +22,18 @@
 </style>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light  text-light " style="background-color: #d9d9d9 !important;">
+<nav class="navbar navbar-expand-lg navbar-light  text-light " style="background-color: #d9d9d9 !important; border-bottom: 1px solid black">
     <div class="container-fluid">
         <a class="navbar-brand d-flex align-items-center" href="#">
             <img src="{{ asset('storage/images/logo.png') }}" alt="Logo" class="d-inline-block align-text-top">
         </a>
 
-        <div class="mx-auto page-title">Accueil</div>
+        <div class="mx-auto page-title border border-dark border-2 rounded-pill text-primary p-2">Accueil</div>
 
         <div class="d-flex align-items-center">
             <div>
-            <span class="">User Name</span><br>
-            <span class="">000000000000</span></div>
+            <span class=" text-primary ">User Name</span><br>
+            <span class=" text-primary border-top border-dark">000000000000</span></div>
             <img src="{{ asset('storage/images/profil.png') }}" alt="User" class="rounded-circle me-2" height="50px" width="50px">
             </div>
         </div>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -18,13 +18,14 @@
 <div class="d-flex">
     <div class="row">
         <!-- Sidebar -->
-        <div class="col-md-3 col-lg-2 p-0 bg-dark text-light w-100" style="background-color: #d9d9d9 !important;">
+        <div class="col-md-3 col-lg-2 p-0 bg-dark text-light w-100" style="background-color: #d9d9d9 !important; border: 1px solid black">
             <div class="d-flex flex-column vh-100 ms-2 sidebar1"><br><br><hr>
-                <div class="list-group flex-grow-1 mt-auto p-3 button1"><br>
+                <div class="list-group flex-grow-1 mt-auto p-3 button1 text-center"><br>
                     <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Accueil</a><br>
-                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Reports</a><br>
+                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Raports</a><br>
                     <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Statistics</a><br>
                     <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Archive</a><br>
+                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #e48e1e !important;">Gestion Foncionnaire</a><br>
                 </div>
                 <div class="mt-auto p-3">
                     <button class="btn btn-danger button2">Logout</button>
